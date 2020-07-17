d5c2a0b37b0025501fe2d31d8edf93d75c5a5ff4
gjy <644459085@qq.com>
2020-07-17 15:04:48 +0800

    gjy的提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,41 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Login from '../components/Login'
+import Home from '../components/Home'
+import Users from '../components/system/Users'
+import Welcome from '../components/Welcome'
+import Roles from '../components/system/Roles'
+import Department from '../components/system/Department'
+import Alerts from '../components/message/Alerts'
+import Urge from '../components/message/Urge'
+import Approve from '../components/message/Approve'
+import Pass from '../components/message/Pass'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/login'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home,
+    redirect: '/welcome',
+    children: [{ path: '/welcome', component: Welcome },
+      { path: '/roles', component: Roles },
+      { path: '/users', component: Users },
+      { path: '/department', component: Department },
+      { path: '/alerts', component: Alerts },
+      { path: '/urge', component: Urge },
+      { path: '/approve', component: Approve },
+      { path: '/pass', component: Pass }
+    ]
   }
 ]
 
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    userInfo: JSON.parse(sessionStorage.getItem('userInfo')) || {}
   },
   mutations: {
+    setUserInfo (state, userInfo) {
+      sessionStorage.setItem('userInfo', JSON.stringify(userInfo))// 将传递的数据先保存到localStorage中
+      state.userInfo = userInfo// 之后才是修改state中的状态
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
+  actions: {},
+  modules: {}
 })
